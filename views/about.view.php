9000ba392264d2eb51dc80d457ea5937cd65ca13
Mackab <itsabeltwitch@outlook.com>
2024-09-30 22:45:10 +0200

    initial about grid

--- views/about.view.php
+++ views/about.view.php
@@ -5,7 +5,14 @@ require 'layout/head.php';
 ?>
     
     <main>
-
+        <div class="about-parent">
+            <div class="about-div1"> </div>
+            <div class="about-div2"> </div>
+            <div class="about-div3"> </div>
+            <div class="about-div4"> </div>
+            <div class="about-div5"> </div>
+            <div class="about-div6"> </div>
+        </div>     
     </main>
 
 <?php
