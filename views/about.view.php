9c6cd3a7c2906e0103d717ccc4b6acdb477594f7
Mackab <itsabeltwitch@outlook.com>
2024-09-30 23:04:20 +0200

    initial grid template

--- views/about.view.php
+++ views/about.view.php
@@ -6,12 +6,24 @@ require 'layout/head.php';
     
     <main>
         <div class="about-parent">
-            <div class="about-div1"> </div>
-            <div class="about-div2"> </div>
-            <div class="about-div3"> </div>
-            <div class="about-div4"> </div>
-            <div class="about-div5"> </div>
-            <div class="about-div6"> </div>
+            <div class="about-div1 card"> 
+                <h1>Wie ben ik?</h1>
+            </div>
+            <div class="about-div2 card"> 
+                <h1>Software Developer & Student</h1>
+            </div>
+            <div class="about-div3 card"> 
+                 <h1>My Skills</h1>
+            </div>
+            <div class="about-div4 card"> 
+                <h1>Curriculum Vitae</h1>
+            </div>
+            <div class="about-div5"> 
+                
+            </div>
+            <div class="about-div6 card"> 
+                <h2>"Inspirtional Quote"</h2>
+            </div>
         </div>     
     </main>
 
