d4c8408d11d5d897cff0f5805056bd19618927ee
ItsAbel <itsabeltwitch@outlook.com>
2024-10-01 12:07:22 +0200

    SIZING

--- views/about.view.php
+++ views/about.view.php
@@ -8,6 +8,8 @@ require 'layout/head.php';
         <div class="about-parent">
             <div class="about-div1 card"> 
                 <h1>Wie ben ik?</h1>
+                <a class="subtext">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    <br> Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</a>
             </div>
             <div class="about-div2 card"> 
                 <h1>Software Developer & Student</h1>
@@ -15,10 +17,10 @@ require 'layout/head.php';
             <div class="about-div3 card"> 
                  <h1>My Skills</h1>
                  <div class="skills">
-                    <i class="devicon-html5-plain subtext"></i>
-                    <i class="devicon-css3-plain subtext"></i>
-                    <i class="devicon-photoshop-plain subtext"></i>
-                    <i class="devicon-git-plain subtext"></i>
+                    <i class="devicon-html5-plain icons"></i>
+                    <i class="devicon-css3-plain icons"></i>
+                    <i class="devicon-photoshop-plain icons"></i>
+                    <i class="devicon-git-plain icons"></i>
                 </div class=skills>
             </div>
             <div class="about-div4 card"> 
@@ -28,7 +30,8 @@ require 'layout/head.php';
                 
             </div>
             <div class="about-div6 card"> 
-                <h2>"Inspirtional Quote"</h2>
+                <h2>"Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    <br> sed do eiusmod tempor."</h2>
             </div>
         </div>     
     </main>
