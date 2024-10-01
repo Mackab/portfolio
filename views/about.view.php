509bb96c89977d4e5b4825d2551c26e78b1bc18d
Mackab <itsabeltwitch@outlook.com>
2024-10-01 21:48:40 +0200

    redo for responsiveness

--- views/about.view.php
+++ views/about.view.php
@@ -5,35 +5,7 @@ require 'layout/head.php';
 ?>
     
     <main>
-        <div class="about-parent">
-            <div class="about-div1 card"> 
-                <h1>Wie ben ik?</h1>
-                <a class="subtext">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                    <br> Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</a>
-            </div>
-            <div class="about-div2 card"> 
-                <h1>Software Developer & Student</h1>
-            </div>
-            <div class="about-div3 card"> 
-                 <h1>My Skills</h1>
-                 <div class="skills">
-                    <i class="devicon-html5-plain icons"></i>
-                    <i class="devicon-css3-plain icons"></i>
-                    <i class="devicon-photoshop-plain icons"></i>
-                    <i class="devicon-git-plain icons"></i>
-                </div class=skills>
-            </div>
-            <div class="about-div4 card"> 
-                <h1>Curriculum Vitae</h1>
-            </div>
-            <div class="about-div5"> 
-                
-            </div>
-            <div class="about-div6 card"> 
-                <h2>"Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                    <br> sed do eiusmod tempor."</h2>
-            </div>
-        </div>     
+        
     </main>
 
 <?php
--- views/index.view.php
+++ views/index.view.php
@@ -5,18 +5,7 @@ require 'layout/head.php';
 ?>
     
     <main>
-        <div class="parent">
-            <div class="div1"> 
-                <img src="../assets/imgs/me.jpg" class="main-picture">
-            </div>
-            <div class="div2"> 
-                <a class="subtitle">Abel<br></a>
-                <a class="title">Mackenbach</a>
-            </div>
-            <div class="div3"> 
-                <a><button class="main-btn">Mijn portfolio</button></a>
-            </div>
-        </div>
+        
     </main>
 
 <?php
