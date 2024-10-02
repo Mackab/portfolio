b4c0a0134da34cebeb2539831ef800a3e7020cff
ItsAbel <itsabeltwitch@outlook.com>
2024-10-02 12:32:18 +0200

    footer start

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
 
-require 'views/index.view.php';
+$requestPage = $_SERVER['REQUEST_URI'];
+
+switch ($requestPage) {
+    case '/':
+        require 'views/index.view.php';
+        break;
+    case '/views/about.view.php':
+        require 'views/about.view.php';
+        break;
+}
--- views/layout/foot.php
+++ views/layout/foot.php
@@ -1,5 +1,9 @@
 <footer>
-
+    <div class="footer-icons">
+        <i class="devicon-github-original subtext icons"></i>
+        <i class="devicon-linkedin-plain subtext icons"></i>
+    </div>
+    <a class="subtext">Copyright Abel Mackenbach©</a>
 </footer>
 </body>
 </html>
