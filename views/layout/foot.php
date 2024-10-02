129b649a1021471dfc3a85a3354a586dc07f624c
ItsAbel <itsabeltwitch@outlook.com>
2024-10-02 12:52:06 +0200

    final touches

--- views/layout/foot.php
+++ views/layout/foot.php
@@ -1,7 +1,7 @@
 <footer>
     <div class="footer-icons">
-        <i class="devicon-github-original subtext icons"></i>
-        <i class="devicon-linkedin-plain subtext icons"></i>
+        <a class="icons" href="https://github.com/Mackab" target="_blank" rel="noopener noreferrer"><i class="devicon-github-original subtext"></i></a>
+        <a class="icons" href="https://www.linkedin.com/in/abel-mackenbach/" target="_blank" rel="noopener noreferrer"><i class="devicon-linkedin-plain subtext"></i></a>
     </div>
     <a class="subtext">Copyright Abel Mackenbach©</a>
 </footer>
