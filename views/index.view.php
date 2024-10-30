bbd14b3bc26ee4391279a43ae8caf55066c65199
Mackab <itsabeltwitch@outlook.com>
2024-10-30 17:10:33 +0100

    responsiveness change

--- views/index.view.php
+++ views/index.view.php
@@ -5,10 +5,12 @@ require 'layout/head.php';
 ?>
     
     <main>
+        <div class="home-container">
         <img src="assets/imgs/me.jpg" class="main-image">
         <div class="index-container">
-            <div><a class="main-text">Abel <br> Mackenbach</a></div>
-            <div><button><a href="../index.php?controller=projects&action=index">Bekijk mijn projecten</a></button></div>
+            <div><a class="main-text">Abel Mackenbach</a></div>
+            <div class="main-btn"><button><a href="../index.php?controller=projects&action=index">Bekijk mijn projecten</a></button></div>
+        </div>
         </div>
     </main>
 
