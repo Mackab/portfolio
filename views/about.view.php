3a846a6e362e9ef29dc64d582d03d3b1f28cd810
Mackab <itsabeltwitch@outlook.com>
2024-10-30 21:48:54 +0100

    about page done actually this time

--- views/about.view.php
+++ views/about.view.php
@@ -5,7 +5,48 @@ require 'layout/head.php';
 ?>
     
     <main>
-        
+    <div class="about-container">
+        <div class="about-intro about-label"> 
+            <h1>Hey, ik ben Abel!</h1>
+            <a class="subtext">Op dit moment studeer Software Development aan het Windesheim in Almere, ik zit op dit moment in het eerste leerjaar. <br>
+            Naast mijn opleiding ben ik groot fan van Formule 1, Rugby en ik ben ook een vrij actieve gamer.
+        </a>
+        </div>
+        <div class="about-picture about-label"> 
+        </div>
+        <div class="about-titles about-label"> 
+            <h1 style="color:rgb(255, 81, 0)">Student & Software Developer</h1>
+        </div>
+        <div class="about-experiences about-label">
+            <h1 class="experience-title">Mijn Werkervaring</h1> 
+            <ol>
+            <li class="experience-list-item">
+                    <h3 class="experience-li-title">DUWO - Stage - Medewerker V&I<h3/>
+                        <p class="experience-li-subtext subtext">Sep 2021 - Jan 2022 | Feb 2023 - Jun 2023</p>
+                </li>
+                <li class="experience-list-item">
+                    <h3 class="experience-li-title">DUWO - Zomerbaan - Medewerker V&I<h3/>
+                        <p class="experience-li-subtext subtext">Jun 2022 - Aug 2022</p>
+                </li>
+                <li class="experience-list-item">
+                    <h3 class="experience-li-title">MBO College Almere & Poort - Studentenraad<h3/>
+                        <pVoorzitter</p>
+                        <p class="experience-li-subtext subtext">April 2020 - April 2021</p>
+                        <p>Secretaris</p>
+                        <p class="experience-li-subtext subtext">Sep 2018 - Mrt 2020</p>
+                </li>
+            </ol>
+        </div>
+        <div class="about-quote about-label"> 
+            <h1>"Value Loyalty Above All Else"</h1>
+        </div>
+        <div class="about-skills about-label">
+            <h1 class="skills-title">Mijn Skills</h1> 
+            <i class="devicon-html5-plain icons subtext"></i>
+            <i class="devicon-css3-plain icons subtext"></i>
+            <i class="devicon-photoshop-plain icons subtext"></i>
+        </div>
+    </div>
     </main>
 
 <?php
