0e050535c8e7765ac72c64ce7ef040839a38ba8a
Mackab <itsabeltwitch@outlook.com>
2024-10-30 13:36:07 +0100

    front page complete

--- views/index.view.php
+++ views/index.view.php
@@ -6,6 +6,10 @@ require 'layout/head.php';
     
     <main>
         <img src="assets/imgs/me.jpg" class="main-image">
+        <div class="index-container">
+            <div><a class="main-text">Abel <br> Mackenbach</a></div>
+            <div><button><a href="../index.php?controller=projects&action=index">Bekijk mijn projecten</a></button></div>
+        </div>
     </main>
 
 <?php
